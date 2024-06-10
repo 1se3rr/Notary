73a63172ef51520b35f23e52e217e8fe693c7e1d
Анастасия <anastasiamyslivceva7@gmail.com>
2024-06-10 23:29:51 +0300

    Add files via upload

--- admin.py
+++ admin.py
@@ -6,12 +6,14 @@ from config import app, db
 from models import Category, NotaryAction, TariffType, TariffPrice, Title, Service, ArticleNorm
 from flask_login import current_user
 
+
 class AdminMixin:
     def is_accessible(self):
         return current_user.is_authenticated and current_user.username == 'admin'
 
     def inaccessible_callback(self, name, **kwargs):
         return redirect(url_for('login'))
+
 
 class MyModelView(AdminMixin, ModelView):
     form_overrides = {
@@ -40,18 +42,34 @@ class MyModelView(AdminMixin, ModelView):
         }
     }
 
+
 class NotaryActionView(MyModelView):
     form_columns = ['name', 'category_id', 'description']
     column_labels = dict(name='Наименование', category_id='Категория', description='Описание')
+
+    def on_model_change(self, form, model, is_created):
+        """Обрабатывает данные из формы и записывает валидные данные"""
+        model.category_id = form.data['category_id'].id_category
+
 
 class TariffPriceView(MyModelView):
     form_columns = ['price', 'description', 'tariff_type_id', 'service']
     column_labels = dict(price='Цена', description='Описание', tariff_type_id='Тип тарифа', service='Услуга')
 
+    def on_model_change(self, form, model, is_created):
+        """Обрабатывает данные из формы и записывает валидные данные"""
+        model.tariff_type_id = form.data['tariff_type_id'].id_tariff_types
+        model.id_service = form.data['service'].id_service
+
+
 class ServiceView(MyModelView):
     form_columns = ['name', 'title_id', 'norm_id']
     column_labels = dict(name='Наименование', title_id='Название', norm_id='Норма')
 
+    def on_model_change(self, form, model, is_created):
+        """Обрабатывает данные из формы и записывает валидные данные"""
+        model.title_id = form.data['title_id'].id_title
+        model.norm_id = form.data['norm_id'].id_norm
 
 
 class MyAdminIndexView(AdminMixin, AdminIndexView):
@@ -59,7 +77,9 @@ class MyAdminIndexView(AdminMixin, AdminIndexView):
     def index(self):
         return self.render('admin/index.html')
 
+
 admin = Admin(app, name='Админ Панель', template_mode='bootstrap3', index_view=MyAdminIndexView(), url='/admin_panel')
+
 
 admin.add_view(MyModelView(Category, db.session, name='Категории'))
 admin.add_view(NotaryActionView(NotaryAction, db.session, name='Нотариальные Действия'))
