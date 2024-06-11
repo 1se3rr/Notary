774842d8f255526a8fbeeeb2d0e0739a00dcee0a
Анастасия <anastasiamyslivceva7@gmail.com>
2024-06-11 22:53:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+from flask_talisman import Talisman
+from flask_wtf import CSRFProtect
 
 from views import *
 from admin import *
 
-#
+csrf = CSRFProtect(app)
+
+csp = {
+    'default-src': ["'self'"],
+    'script-src': ["'report-sample'", "'self'"],
+    'style-src': ["'report-sample'", "'self'"],
+    'object-src': ["'none'"],
+    'base-uri': ["'self'"],
+    'connect-src': ["'self'"],
+    'font-src': ["'self'"],
+    'frame-src': ["'self'"],
+    'img-src': ["'self'"],
+    'manifest-src': ["'self'"],
+    'media-src': ["'self'"],
+    'report-uri': ['https://6666ef9dd528e3ceb6b0c68a.endpoint.csper.io/?v=2'],
+    'worker-src': ["'none'"],
+    'frame-ancestors': ["'self'"],
+    'form-action': ["'self'"]
+}
+
+# Дополнительные заголовки безопасности
+talisman = Talisman(
+    app,
+    content_security_policy=csp,
+    content_security_policy_nonce_in=['script-src', 'style-src'],
+    frame_options='DENY',
+    strict_transport_security=True,
+    strict_transport_security_preload=True,
+    strict_transport_security_max_age=31536000,
+    strict_transport_security_include_subdomains=True,
+    x_content_type_options=True,
+    x_xss_protection=True
+)
 
 if __name__ == '__main__':
     with app.app_context():
